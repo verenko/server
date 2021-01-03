517f6b367ec9e80ba49dce4a72cad8bd08c63b89
verenko <Verenko2000@gmail.com>
2021-01-03 19:56:55 +0200

    ap

--- config.sh
+++ config.sh
@@ -6,19 +6,40 @@ apt update
 apt upgrade -y
 
 apt install vim -y
-apt-get install vsftpd -y
 
-useradd -m sammy
-
+#user add
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 useradd -m -p "$pass" "$username"
 
+#FTP config
+apt-get install vsftpd -y
 mkdir /home/$username/ftp
-chown $username:$username /home/$username/ftp
+chown nobody:nogroup /home/$username/ftp
+chmod a-w /home/$username/ftp
+
+mkdir /home/$username/ftp/files
+chown $username:$username /home/alex/ftp/files
+rm /etc/vsftpd.conf
+cp /root/server/conf /etc
+service vsftpd restart
+
+
 
 #ssh config
 rm /etc/ssh/sshd_config
-cp /root/server/
+cp /root/server/sshd_config /etc/ssh/
+service ssh restart
+
+
+
+
+
+
+
+
+
+
+
 
 #zsh install
 apt-get install zsh -y
